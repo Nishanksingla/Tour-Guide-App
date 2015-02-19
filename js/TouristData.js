19394b242cfdc0ab87ff360c3b13d1701f15b7a8
Nishank singla <nishanksingla@gmail.com>
2015-02-19 20:11:50 +0530

    remove node modules

---
+++ js/TouristData.js
@@ -0,0 +1,10 @@
+﻿var placesData = {
+    'New Delhi': {
+        'LotusTemple': {
+            'Name': 'Lotus Temple, New Delhi India',
+            'Panoid': 'WdBZGlAP9jwAAAGu41fwjQ',
+            'Information':
+        },
+        'QutubMinar': {}
+    }
+};
